39f3d6dc7b473fd7f28d85508749e4bb58fd588d
laurenmacpherson <60455659+laurenmacpherson@users.noreply.github.com>
2020-12-06 23:40:16 -0800

    Update login.php
    How to redirect: lines 51, 52

--- login-reg/login.php
+++ login-reg/login.php
@@ -1,7 +1,12 @@
 <?php
+$errorMessage = "";
+$conn = mysqli_connect("localhost", "root", "", "time_crunch_planner");
 if (isset($_POST["username"]) && isset($_POST["password"])){
-
-      $conn = mysqli_connect("localhost", "root", "", "userbank");
+      if(isset($_SESSION['username'])){ //if the login is broken, check that this didn't break it
+            session_start();
+            session_unset();
+            session_destroy();
+      }
 
       if(!$conn){
           die("Connection failed" . mysqli_connect_error());
@@ -9,47 +14,52 @@ if (isset($_POST["username"]) && isset($_POST["password"])){
 
       $username = $_POST['username'];
       $pin = $_POST['password'];
+
       if (empty($username) || empty($pin)){
             echo "variables empty";
             exit();
       }
       else {
-            $sql = "SELECT * FROM useraccounts WHERE userID=? OR email=?;";
+            $sql = "SELECT * FROM useraccounts WHERE username=?;";
             $stmt = mysqli_stmt_init($conn);
+
             if(!mysqli_stmt_prepare($stmt, $sql)) {
-                  echo "SQL error";
+                  echo "first if error";
                   exit();
             }
             else{
-                  mysqli_stmt_bind_param($stmt, "si", $username, $username);
+                  //mysqli_stmt_bind_param($stmt, "si", $username, $username);
+                  mysqli_stmt_bind_param($stmt, "s", $username);
                   mysqli_stmt_execute($stmt);
                   $result = mysqli_stmt_get_result($stmt);
 
                   if ($row = mysqli_fetch_assoc($result)){
                         $pwdCheck = password_verify($pin, $row['pin']);
-                        echo ( 'Pin: '.$row['pin'] );
                         if(!($pin == $row['pin'])){
-                              echo "Error: password incorrect.";
+                              $errorMessage = "<b>Error:</b> Sign in failed. Incorrect username or password.";
                         }
                         else{
                               session_start();
                               $_SESSION['userID']= $row['userID'];
-
-                              echo("Session ID: " .$_SESSION['userID']);
                               $_SESSION['fname']= $row['fname'];
                               $_SESSION['lname']= $row['lname'];
                               $_SESSION['username']= $row['username'];
                               $_SESSION['email']= $row['email'];
 
+                              //echo "Success! Logged in as " .$_SESSION['fname']. " " .$_SESSION['lname']. ". Your user ID is " .$_SESSION['userID']. " and your email is " .$_SESSION['email']. ".";
+
+                              //header('Location: /test_userpage.php');
+                              /*NOTE: Use the above line to redirect to the page needed, i.e. Location: /homepage.php*/
                         }
                   }
                   else{
-                        echo "Data match error.";
+                        $errorMessage = "Error: Sign in failed. User entered does not have an account.";
                   }
             }
       }
 }
 else {
+      echo ""; //will remove this once I figure some stuff out
 }
 ?>
 
@@ -57,55 +67,16 @@ else {
   <head>
     <meta charset="utf-8">
     <title>Login</title>
-    <link rel="stylesheet" href="homepage.css">
-      </head>
-
-    <body><!This is the login page>
-     <!MENU BAR>
-     <div id="mySidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="">Checking Account</a>
-        <a href="">Savings Account</a>
-        <a href="">Investing</a>
-        <a href="">Better Money Habits</a>
-     </div>
-
-     <div id="main">
-
-       <div class="header">
-         <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; MENU</span>
-         <!TOP BAR>
-         <a href="homepage.php", class="logo", style="color: #FFFFE0">Bank Name</a>
-      </div>
-
-      <div class="topnav">
-        <a class="active" href="homepage.php">Home</a>
-        <a href="homepage.php#news">News</a>
-        <a href="homepage.php#contact">Contact</a>
-        <a href="homepage.php#about">About</a>
-        <div class="header-right">
-          <a class="active" href="register.php">Register</a>
-        </div>
-      </div>
-      <br>
 
 
-  <div id= "form">
-        <h2>Login</h2>
-          <form action = "login.php" method="post">
-                  <input type= "text" name = "username" placeholder = "username/email">
-                  <input type= "password" name = "password" placeholder = "pin">
-            <button type = "submit" name= "login-submit">Login</button>
-      	</form>
-            <main>
-                  <?php
-                        if(isset($_SESSION['username'])){
-                              echo '<p>Logged IN</p>';
-                        }
-                        else{
-                              echo '<p>Logged OUT</p>';
-                        }
-                  ?>
-            </main>
-</div>
+                         <h2>Login</h2>
+                              <p style = "color: red; text-align: center;" ><?php echo $errorMessage ?></p>
+                            <form action = "login.php" method="post">
+                                    <p><input type= "text" name = "username" placeholder = "username" required></p>
+                                    <p><input type= "password" name = "password" placeholder = "pin" required></p>
+                              <p><button type = "submit" name= "login-submit">Login</button></p>
+                              </form>
+                              <button onclick="document.location='register.php'">Register here</button>
+                        </div>
+            </div>
 </body>
