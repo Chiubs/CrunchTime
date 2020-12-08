79313409d3c23704c2a2c5d6d1dbb09eef4ad189
laurenmacpherson <60455659+laurenmacpherson@users.noreply.github.com>
2020-12-07 21:23:25 -0800

    Add files via upload
    Working: All of the calendar functions 
    Not working: session variables, such as user ID (vital)
    Need to: create prompt for time of day that the event occurs, and how long.

---
+++ Latest_Code/load.php
@@ -0,0 +1,29 @@
+<?php
+
+//load.php
+
+$connect = new PDO('mysql:host=localhost;dbname=time_crunch_planner', 'root', '');
+
+$data = array();
+
+$query = "SELECT * FROM events ORDER BY eventNum";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+
+foreach($result as $row)
+{
+ $data[] = array(
+  'start'   => $row["date"],
+  'end'   => $row["end"],
+  'id'   => $row["eventNum"],
+  'title'   => $row["eventName"]
+ );
+}
+
+echo json_encode($data);
+
+?>
